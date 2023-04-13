57699ce583c440a337940f68179bf1cb7f4bb96c
Kim-EunSu <kes73640@gmail.com>
2023-04-13 13:15:22 +0900

    schema 정의

---
+++ sanity-studio/schemas/post.js
@@ -0,0 +1,55 @@
+export default {
+  title: 'Post',
+  name: 'post',
+  type: 'document',
+  fields: [
+    {
+      title: 'Author',
+      name: 'author',
+      type: 'reference',
+      to: [{type: 'user'}],
+    },
+    {
+      title: 'Photo',
+      name: 'photo',
+      type: 'image',
+    },
+    {
+      title: 'Likes',
+      name: 'likes',
+      type: 'array',
+      of: [
+        {
+          type: 'reference', //다른 사람들의 reference를 참조
+          to: [{type: 'user'}], // 배열안의 object형식으로 참조
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Comments',
+      name: 'comments',
+      type: 'array',
+      of: [
+        {
+          title: 'Comment',
+          name: 'comment',
+          type: 'document',
+          fields: [
+            {
+              title: 'Author',
+              name: 'author',
+              type: 'reference',
+              to: [{type: 'user'}],
+            },
+            {
+              title: 'Comment',
+              name: 'comment',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
---
+++ sanity-studio/schemas/user.js
@@ -0,0 +1,63 @@
+export default {
+  title: 'User',
+  name: 'user',
+  type: 'document',
+  fields: [
+    {
+      title: 'Username',
+      name: 'username',
+      type: 'string',
+    },
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'string',
+    },
+    {
+      title: 'Following',
+      name: 'following',
+      type: 'array',
+      of: [
+        {
+          type: 'reference', //다른 사람들의 reference를 참조
+          to: [{type: 'user'}], // 배열안의 object형식으로 참조
+        },
+      ],
+      validation: (Rule) => Rule.unique(), //중복을 허용해주고싶지않으므로 validation으로
+    },
+    {
+      title: 'Followers',
+      name: 'followers',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Bookmarks',
+      name: 'bookmarks',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'post'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+  ],
+}
