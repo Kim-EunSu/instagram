480fdf3c43204ad5a49fb0aaa4c7fe0c21654df2
Kim-EunSu <kes73640@gmail.com>
2023-04-13 14:07:42 +0900

    프리뷰 추가

--- sanity-studio/schemas/post.js
+++ sanity-studio/schemas/post.js
@@ -52,4 +52,21 @@ export default {
       ],
     },
   ],
+  preview: {
+    select: {
+      //comments배열의 있는 0번째 comment를 가져올 것
+      title: 'comments.0.comment',
+      authorName: 'author.name',
+      authorUsername: 'author.username',
+      media: 'photo',
+    },
+    prepare(selection) {
+      const {title, authorName, authorUsername, media} = selection
+      return {
+        title,
+        subtitle: `by ${authorName} (${authorUsername})`,
+        media,
+      }
+    },
+  },
 }
