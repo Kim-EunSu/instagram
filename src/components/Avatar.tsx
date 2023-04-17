65512d2b58f9a6587e66add7121aec2af35b31d9
Kim-EunSu <kes73640@gmail.com>
2023-04-17 13:24:29 +0900

    로그인/아바타 구현

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -10,7 +10,7 @@ export default function Avatar({ image }: Props) {
   return (
     <div className="w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300">
       {/* image가 없다면 undefined를 사용 */}
-      {/* eslint-disable-next-line @next/next/no-img-element */}
+      {/* eslint-disable-next-line @next/next/no-img-element  */}
       <img
         className="rounded-full p-[0.1rem]"
         src={image ?? "undefined"}
