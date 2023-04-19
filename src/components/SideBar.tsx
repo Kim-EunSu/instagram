89a3b76791a0987869aae8e4494bc92e558a2731
Kim-EunSu <kes73640@gmail.com>
2023-04-19 11:05:53 +0900

    사이드바 구현/스타일

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -11,14 +11,16 @@ export default function SideBar({ user: { name, username, image } }: Props) {
       <div className="flex items-center ">
         {image && <Avatar image={image} />}
         <div className="ml-4">
-          <p className="text-lg">{username}</p>
-          <p className="font-bold text-neutral-500 leading-4">{name}</p>
+          <p className="font-bold">{username}</p>
+          <p className="text-lg text-neutral-500 leading-4">{name}</p>
         </div>
       </div>
       <p className="text-sm text-neutral-500 mt-8">
         About • Help • API • Jobs • Privacy • Language
       </p>
-      <p className="font-bold text-neutral-500 mt-8">@Copyright Instagram</p>
+      <p className="font-bold text-sm text-neutral-500 mt-8">
+        @Copyright Instagram
+      </p>
     </>
   );
 }
