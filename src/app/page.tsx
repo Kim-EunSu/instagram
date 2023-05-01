2626648d90e119a17f37e57a17673195789567ac
Kim-EunSu <kes73640@gmail.com>
2023-05-01 11:06:42 +0900

    FollowingBar 스타일링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,12 @@ export default async function HomePage() {
   }
 
   return (
-    <section className="flex flex-col md:flex-row max-w-[850px] p-4 ">
-      <div className="w-full basis-3/4 bg-neutral-50">
+    <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4 ">
+      <div className="w-full basis-3/4 min-w-0">
         <FollowingBar />
         <PostList />
       </div>
-      <div className="basis-1/4">
+      <div className="basis-1/4 ml-8">
         <SideBar user={user} />
       </div>
     </section>
--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -2,11 +2,45 @@
 
 import { DetailUser } from "@/model/user";
 import useSWR from "swr";
+import PropagateLoader from "react-spinners/PropagateLoader";
+import Link from "next/link";
+import Avatar from "./Avatar";
 
 export default function FollowingBar() {
   const { data, error, isLoading: loading } = useSWR<DetailUser>("/api/me");
 
-  const users = data?.following;
+  //const users = data?.following;
 
-  return <section>{loading ? <></> : <></>}</section>;
+  const users = data?.following && [
+    ...data.following,
+    ...data.following,
+    ...data.following,
+  ];
+
+  return (
+    <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
+      {loading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ul className="w-full flex gap-2">
+          {users.map(({ image, username }) => (
+            <li key={username}>
+              <Link
+                className="flex flex-col items-center w-20"
+                href={`/user/${username}`}
+              >
+                <Avatar image={image} highlight />
+                <p className=" w-full text-sm text-center text-ellipsis overflow-hidden">
+                  {username}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
